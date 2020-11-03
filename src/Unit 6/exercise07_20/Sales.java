2e8b5eb7c39c44b7209fabaab593d768834a6fee
BVwj <brandonvue1231@gmail.com>
2020-11-02 23:51:52 -0600

    feat: Adding UNFINISHED Unit 6

---
+++ src/Unit 6/exercise07_20/Sales.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+public class Sales {
+
+    private int user_id;
+    private int product_id;
+    private int amount_sold;
+    private int[][] sales;
+    private int[] list;
+    private int col_sum;
+
+    private int item_one_sold = 0;
+    private int item_two_sold = 0;
+    private int item_three_sold = 0;
+    private int item_four_sold = 0;
+
+    // Constructor
+
+    public Sales() {
+        // Instantiate a new Array called... sales
+        sales = new int[4][5];
+        list = new int[4];
+
+        user_id = -1;
+        product_id = -1;
+        amount_sold = 0;
+    }
+
+    // Sales array
+
+    public int[][] getSales() {
+        return sales;
+    }
+
+    // User ID
+
+    public void setUserId(int UserId) {
+        this.user_id = UserId;
+    }
+
+    public int getUserId() {
+        return user_id;
+    }
+
+    // Product ID
+
+    public void setProductId(int ProductId) {
+        this.product_id = ProductId;
+    }
+
+    public int getProductId() {
+        return product_id;
+    }
+
+    // Quantity Sold
+
+    public void setAmountSold(int amountSold) {
+        this.amount_sold = amountSold;
+    }
+
+    public int getAmountSold() {
+        return amount_sold;
+    }
+
+    // Function to take in user input, product number, and quantity sold,
+    // then passes that to an array position, finally, it assigns it using... [i][j] += quantity
+    public String setItemQuantitySold(int id, int product_num, int quantity_sold) {
+        String response = "Unable to find item, check if your Employee or Product ID is correct and exists!";
+
+        if (id > 3 || id < 0) {
+            response = "No Employee was found with this ID.";
+        } else if (product_num > 4 || product_num < 0) {
+            response = "No Product was found with this ID.";
+        } else if (quantity_sold < 0) {
+            response = "Invalid quantity amount, cannot be negative.";
+        } else {
+            response = "Information has been recorded! Thank you for your cooperation.";
+            sales[id][product_num] += quantity_sold;
+        }
+
+        return response;
+    }
+
+    // Create function to GET quantity sold
+    public int getItemQuantitySold(int id, int product_num) {
+        try {
+            return sales[id][product_num];
+        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println(e);
+            return -1;
+        }
+    }
+
+    public int[] addToColumnSum(int user_id, int quantity_amount) {
+        switch (user_id) {
+            case 0:
+                item_one_sold += quantity_amount;
+                list[0] += item_one_sold;
+                break;
+            case 1:
+                item_two_sold += quantity_amount;
+                list[1] += item_two_sold;
+                break;
+            case 2:
+                item_three_sold += quantity_amount;
+                list[2] += item_three_sold;
+                break;
+            case 3:
+                item_four_sold += quantity_amount;
+                list[3] += item_four_sold;
+                break;
+        }
+        return list;
+    }
+
+    public int[] getList() {
+        return list;
+    }
+
+}
---
+++ src/Unit 6/exercise07_20/SalesTest.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class SalesTest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Sales s = new Sales();
+
+        // Loop vars
+        int input;
+        int answer;
+        int quantity;
+        int[][] array = s.getSales();
+        int[] value = s.getList();
+        int[] col_sum = new int[4];
+
+        // Loop that asks for user input... id, product number... Another Loop will use i and j variables
+        // Which is passed into getUserId(i) & getProductId(j) as i and j
+        while (true) {
+            System.out.println("");
+            System.out.println("+++++ Employee Sales Database +++++");
+
+            // Asks user for input
+            System.out.println("Enter -1 to exit when asked to input a number.");
+            System.out.println("");
+
+            System.out.println("+++++ Employee ID +++++");
+            System.out.println("Enter your Employee ID Number (0 - 3): ");
+            input = sc.nextInt();
+            if (input == -1) {
+                System.out.println("");
+                System.out.println("ABRUPT STOP!");
+                System.out.println("Reason: User Exited...");
+                System.out.println("");
+                break;
+            } else {
+                s.setUserId(input);
+            }
+
+            System.out.println("");
+            System.out.println("+++++ Product ID +++++");
+            System.out.println("Enter the Product ID Number (0 - 4): ");
+            answer = sc.nextInt();
+            if (answer == -1) {
+                System.out.println("");
+                System.out.println("ABRUPT STOP!");
+                System.out.println("Reason: User Exited...");
+                System.out.println("");
+                break;
+            } else {
+                s.setProductId(answer);
+            }
+
+            System.out.println("");
+
+            if (s.getItemQuantitySold(s.getUserId(), s.getProductId()) == -1) {
+                System.out.println("ABRUPT STOP!");
+                System.out.println("ERROR: No Employee # can exceed 3");
+                System.out.println("ERROR: No Product # can exceed 4");
+                System.out.println("");
+                break;
+            }
+
+            System.out.println("+++++ Amount Sold +++++");
+            System.out.println("How many did you sell? ");
+            quantity = sc.nextInt();
+            if (quantity == -1) {
+                System.out.println("");
+                System.out.println("ABRUPT STOP!");
+                System.out.println("Reason: User Exited...");
+                System.out.println("");
+                break;
+            } else {
+                s.setAmountSold(quantity);
+            }
+
+            value = s.addToColumnSum(s.getUserId(), s.getItemQuantitySold(s.getUserId(), s.getProductId()));
+            System.out.println("");
+            System.out.println(s.setItemQuantitySold(s.getUserId(), s.getProductId(), s.getAmountSold()));
+            System.out.println("");
+
+        }
+
+        // Calls function
+        // Loop that outputs data onto a table
+        for (int i = 0; i < array.length; i++) {
+            int row_sum = 0;
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf(array[i][j] + "\t");
+                row_sum += array[i][j];
+
+                if (j == 4) {
+                    System.out.print("+ " + row_sum);
+                }
+            }
+
+            if (i == 3) {
+                System.out.println();
+                System.out.println("+ + + + + + + + + + + + + + + + + + + + +");
+                for (int count = 0; count < 4; count++) {
+                    col_sum[count] = value[count];
+                }
+                for (int count = 0; count < 4; count++) {
+                    System.out.printf(col_sum[count] + "\t");
+                }
+            }
+            System.out.println("");
+        }
+
+        // Reset User and Product information
+        System.out.println("");
+        System.out.println("Resetting Input Data...");
+        s.setUserId(-1);
+        s.setProductId(-1);
+        System.out.println("Successfully Reset Input Data!");
+
+        System.out.println("");
+        System.out.println("You are now logged out.");
+        System.out.println("Restart App to log back in.");
+
+    }
+
+}
